50183202b160b69a5787d3948bf38c50b5b47979
Richard Todd <rwtodd@users.noreply.github.com>
2026-03-07 21:18:18 -0600

    updated to the current Go.Forth library version

--- binfind/go-version/go.mod
+++ binfind/go-version/go.mod
@@ -1,3 +1,5 @@
 module org.rwtodd/binfind
 
 go 1.25.5
+
+require github.com/rwtodd/Go.Forth v0.0.0-20260308025412-5328bfc63f4a
--- binfind/go-version/main.go
+++ binfind/go-version/main.go
@@ -10,6 +10,8 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+
+	"github.com/rwtodd/Go.Forth/forth"
 )
 
 const (
@@ -39,6 +41,22 @@ func main() {
 	contextSize := flag.Int("c", 0, "Minimum context in bytes")
 	noColor := flag.Bool("nocolor", false, "Use plain output with no ANSI codes")
 	alignPara := flag.Bool("align", false, "Align output to 16-byte paragraphs (0x10)")
+	forthExpr := flag.String("expr", "", "Forth code to run on each match")
+	forthScript := flag.String("script", "", "File containing Forth code to run on each match")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: binfind [-l hex | -r regex | -a ascii] [-c context] [--nocolor] [--align] [--expr code] [--script file] <files>\n\n")
+		fmt.Fprintf(os.Stderr, "Options:\n")
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\nForth expressions must leave an XT (execution token) on the stack.\n")
+		fmt.Fprintf(os.Stderr, "The XT is called on each match with access to:\n")
+		fmt.Fprintf(os.Stderr, "  data ( -- bytes)        Return matched file contents\n")
+		fmt.Fprintf(os.Stderr, "  offset ( -- n)          Get current match offset\n")
+		fmt.Fprintf(os.Stderr, "  offset! ( n -- )        Set match offset\n")
+		fmt.Fprintf(os.Stderr, "  length ( -- n)          Get current match length\n")
+		fmt.Fprintf(os.Stderr, "  length! ( n -- )        Set match length\n")
+		fmt.Fprintf(os.Stderr, "  output-match ( -- )     Print the match using current settings\n")
+	}
 
 	flag.Parse()
 	files := flag.Args()
@@ -46,7 +64,7 @@ func main() {
 	wantsANSI := !*noColor // if they don't want no color, give them color
 
 	if len(files) == 0 || (*hexPattern == "" && *regPattern == "" && *ascPattern == "") {
-		fmt.Println("Usage: binfind [-l hex | -r regex | -a ascii] [-c context] [--nocolor] [--align] <files>")
+		flag.Usage()
 		os.Exit(1)
 	}
 
@@ -72,6 +90,84 @@ func main() {
 	}
 
 	multiFile := len(files) > 1
+	var currentFname string
+	var currentData []byte
+	var currentOffset int
+	var currentLength int
+
+	vm := forth.NewVM(strings.NewReader(""), os.Stdout)
+	vm.Define(forth.NewNativeWord("data", func(v *forth.VM) error {
+		v.Push(currentData)
+		return nil
+	}))
+	vm.Define(forth.NewNativeWord("offset", func(v *forth.VM) error {
+		v.Push(int64(currentOffset))
+		return nil
+	}))
+	vm.Define(forth.NewNativeWord("offset!", func(v *forth.VM) error {
+		oRaw, err := v.Pop()
+		if err != nil {
+			return err
+		}
+		offset, ok := oRaw.(int64)
+		if !ok {
+			return fmt.Errorf("offset must be an integer")
+		}
+		currentOffset = int(offset)
+		return nil
+	}))
+	vm.Define(forth.NewNativeWord("length", func(v *forth.VM) error {
+		v.Push(int64(currentLength))
+		return nil
+	}))
+	vm.Define(forth.NewNativeWord("length!", func(v *forth.VM) error {
+		lRaw, err := v.Pop()
+		if err != nil {
+			return err
+		}
+		length, ok := lRaw.(int64)
+		if !ok {
+			return fmt.Errorf("length must be an integer")
+		}
+		currentLength = int(length)
+		return nil
+	}))
+	vm.Define(forth.NewNativeWord("output-match", func(v *forth.VM) error {
+		printMatch(currentData, currentOffset, currentOffset+currentLength, *contextSize, currentFname, multiFile, wantsANSI, *alignPara)
+		return nil
+	}))
+
+	var forthCode string = "[ output-match ]"
+	if *forthScript != "" {
+		err := vm.Load(*forthScript)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not load script file: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+
+   	 	if *forthExpr != "" {
+			forthCode = *forthExpr
+		}
+		err := vm.Eval(forthCode)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not evaluate FORTH: %v\n", err)
+			os.Exit(1)
+		}
+	} 
+
+	xtRaw, err := vm.Pop()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Expected an XT on the stack from Forth script\n")
+		os.Exit(1)
+	}
+	userXT, ok := xtRaw.(forth.ExecutionToken)
+	if !ok {
+		fmt.Fprintf(os.Stderr, "Expected an Execution Token (XT), got %T\n", xtRaw)
+		os.Exit(1)
+	}
+	vm.ResetState() // clear up anything the user might have sloppily left behind
+
 	hadErrors := false
 	for _, path := range files {
 		data, err := os.ReadFile(path)
@@ -81,9 +177,18 @@ func main() {
 			continue
 		}
 
+		currentFname = filepath.Base(path)
+		currentData = data
 		matches := re.FindAllIndex(data, -1)
 		for _, m := range matches {
-			printMatch(data, m[0], m[1], *contextSize, filepath.Base(path), multiFile, wantsANSI, *alignPara)
+			currentOffset = m[0]
+			currentLength = m[1] - m[0]
+			err := userXT.Run(vm)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error matching in %s at %x: %v\n", currentFname, m[0], err)
+				hadErrors = true
+			}
+			vm.ResetState()
 		}
 	}
 	if hadErrors { // report to the OS that we errored out
@@ -133,7 +238,6 @@ func printMatch(data []byte, mStart, mEnd, minCtx int, fname string, showFname, 
 		if i == mStart {
 			fmt.Printf("%08x %s<", i, tracker.setColor(wantsANSI))
 		} else {
-			fmt.Print(tracker.setColor(false))
 			fmt.Printf("%08x  ", i)
 		}
 
